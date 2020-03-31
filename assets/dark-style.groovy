b20f911d4428c814e0f607242276a7323edb9226
rvandoosselaer <remy.vand@gmail.com>
2020-03-31 10:06:40 +0200

    reworked the stylesheet

--- assets/dark-style.groovy
+++ assets/dark-style.groovy
@@ -10,16 +10,47 @@ import com.simsilica.lemur.VAlignment
 import com.simsilica.lemur.component.QuadBackgroundComponent
 
 //
+// Colors
+//
+def textColor = color(0.894, 0.894, 0.894, 1)
+def textHighlightColor = color(0.894, 0.894, 0.894, 1)
+def textFocusColor = color(0.894, 0.894, 0.894, 1)
+def textShadowColor = color(0, 0, 0, 0.75f)
+def textShadowHighlightColor = color(0, 0, 0, 0.75f)
+def textShadowFocusColor = color(0, 0, 0, 0.75f)
+
+def buttonColor = color(0.345, 0.345, 0.345, 1)
+def buttonPressedColor = color(0.416, 0.416, 0.416, 1)
+def buttonHighlightColor = color(0.416, 0.416, 0.416, 1)
+
+def buttonPrimaryColor = color(0.298, 0.498, 0.78, 1)
+def buttonPrimaryPressedColor = color(0.443, 0.62, 0.863, 1)
+def buttonPrimaryHighlightColor = color(0.443, 0.62, 0.863, 1)
+
+def listColor = color(0.2, 0.2, 0.2, 1)
+def listSelectionColor = color(0.282, 0.463, 0.718, 0.5)
+
+def textFieldColor = color(0.114, 0.114, 0.114, 1)
+
+def toolBarColor = color(0.137, 0.137, 0.137, 1)
+
+def buttonToolBarPressedColor = color(0.282, 0.463, 0.718, 1)
+def buttonToolBarHighlightColor = color(0.282, 0.463, 0.718, 1)
+def buttonToolBarColor = color(0, 0, 0, 0)
+
+def windowColor = color(0.294, 0.294, 0.294, 1)
+def windowTitleColor = color(0.2, 0.2, 0.2, 1)
+//
 // Global styling
 //
 selector("editor-style") {
     fontSize = 14
-    color = color(0.894, 0.894, 0.894, 1)
-    highlightColor = color(0.894, 0.894, 0.894, 1)
-    focusColor = color(0.894, 0.894, 0.894, 1)
-    shadowColor = color(0, 0, 0, 0.75f)
-    highlightShadowColor = color(0, 0, 0, 0.75f)
-    focusShadowColor = color(0, 0, 0, 0.75f)
+    color = textColor
+    highlightColor = textHighlightColor
+    focusColor = textFocusColor
+    shadowColor = textShadowColor
+    highlightShadowColor = textShadowHighlightColor
+    focusShadowColor = textShadowFocusColor
     shadowOffset = new Vector3f(1, -1, -1);
 }
 
@@ -29,13 +60,13 @@ selector("editor-style") {
 def buttonPressedCmd = new Command<Button>() {
     @Override
     void execute(Button source) {
-        if (source.isPressed()) {
-            source.background = new QuadBackgroundComponent(color(0.416, 0.416, 0.416, 1))
+        if (source.pressed) {
+            source.background = new QuadBackgroundComponent(buttonPressedColor)
         } else {
-            if (source.isHighlightOn()) {
-                source.background = new QuadBackgroundComponent(color(0.416, 0.416, 0.416, 1))
+            if (source.highlightOn) {
+                source.background = new QuadBackgroundComponent(buttonHighlightColor)
             } else {
-                source.background = new QuadBackgroundComponent(color(0.345, 0.345, 0.345, 1))
+                source.background = new QuadBackgroundComponent(buttonColor)
             }
         }
     }
@@ -43,10 +74,10 @@ def buttonPressedCmd = new Command<Button>() {
 def buttonHoverCmd = new Command<Button>() {
     @Override
     void execute(Button source) {
-        if (source.isHighlightOn()) {
-            source.background = new QuadBackgroundComponent(color(0.416, 0.416, 0.416, 1))
+        if (source.highlightOn) {
+            source.background = new QuadBackgroundComponent(buttonHighlightColor)
         } else {
-            source.background = new QuadBackgroundComponent(color(0.345, 0.345, 0.345, 1))
+            source.background = new QuadBackgroundComponent(buttonColor)
         }
     }
 }
@@ -58,7 +89,7 @@ def buttonCmds = [
 ]
 
 selector("button", "editor-style") {
-    background = new QuadBackgroundComponent(color(0.345, 0.345, 0.345, 1))
+    background = new QuadBackgroundComponent(buttonColor)
     buttonCommands = buttonCmds
     textHAlignment = HAlignment.Center
     textVAlignment = VAlignment.Center
@@ -70,13 +101,13 @@ selector("button", "editor-style") {
 def primaryButtonPressedCmd = new Command<Button>() {
     @Override
     void execute(Button source) {
-        if (source.isPressed()) {
-            source.background = new QuadBackgroundComponent(color(0.443, 0.62, 0.863, 1))
+        if (source.pressed) {
+            source.background = new QuadBackgroundComponent(buttonPrimaryPressedColor)
         } else {
-            if (source.isHighlightOn()) {
-                source.background = new QuadBackgroundComponent(color(0.443, 0.62, 0.863, 1))
+            if (source.highlightOn) {
+                source.background = new QuadBackgroundComponent(buttonPrimaryHighlightColor)
             } else {
-                source.background = new QuadBackgroundComponent(color(0.298, 0.498, 0.78, 1))
+                source.background = new QuadBackgroundComponent(buttonPrimaryColor)
             }
         }
     }
@@ -84,10 +115,10 @@ def primaryButtonPressedCmd = new Command<Button>() {
 def primaryButtonHoverCmd = new Command<Button>() {
     @Override
     void execute(Button source) {
-        if (source.isHighlightOn()) {
-            source.background = new QuadBackgroundComponent(color(0.443, 0.62, 0.863, 1))
+        if (source.highlightOn) {
+            source.background = new QuadBackgroundComponent(buttonPrimaryHighlightColor)
         } else {
-            source.background = new QuadBackgroundComponent(color(0.298, 0.498, 0.78, 1))
+            source.background = new QuadBackgroundComponent(buttonPrimaryColor)
         }
     }
 }
@@ -98,7 +129,7 @@ def primaryButtonCmds = [
         (ButtonAction.HighlightOff): [primaryButtonHoverCmd]
 ]
 selector("primary-button", "editor-style") {
-    background = new QuadBackgroundComponent(color(0.298, 0.498, 0.78, 1))
+    background = new QuadBackgroundComponent(buttonPrimaryColor)
     buttonCommands = primaryButtonCmds
     textHAlignment = HAlignment.Center
     textVAlignment = VAlignment.Center
@@ -108,7 +139,7 @@ selector("primary-button", "editor-style") {
 // Listbox
 //
 selector("list.selector", "editor-style") {
-    background = new QuadBackgroundComponent(color(0.282, 0.463, 0.718, 0.5))
+    background = new QuadBackgroundComponent(listSelectionColor)
     background.material.material.additionalRenderState.blendMode = BlendMode.AlphaAdditive;
 }
 
@@ -116,26 +147,26 @@ selector("list.selector", "editor-style") {
 // TextField
 //
 selector("textField", "editor-style") {
-    background = new QuadBackgroundComponent(color(0.114, 0.114, 0.114, 1))
+    background = new QuadBackgroundComponent(textFieldColor)
 }
 
 //
 // Toolbar
 //
 selector("toolbar", "editor-style") {
-    background = new QuadBackgroundComponent(color(0.137, 0.137, 0.137, 1))
+    background = new QuadBackgroundComponent(toolBarColor)
 }
 
 def toolbarPressedCmd = new Command<Button>() {
     @Override
     void execute(Button source) {
-        if (source.isPressed()) {
-            source.background = new QuadBackgroundComponent(color(0.282, 0.463, 0.718, 1))
+        if (source.pressed) {
+            source.background = new QuadBackgroundComponent(buttonToolBarPressedColor)
         } else {
-            if (source.isHighlightOn()) {
-                source.background = new QuadBackgroundComponent(color(0.282, 0.463, 0.718, 1))
+            if (source.highlightOn) {
+                source.background = new QuadBackgroundComponent(buttonToolBarHighlightColor)
             } else {
-                source.background = null
+                source.background = new QuadBackgroundComponent(buttonToolBarColor)
             }
         }
     }
@@ -143,14 +174,13 @@ def toolbarPressedCmd = new Command<Button>() {
 def toolbarHoverCmd = new Command<Button>() {
     @Override
     void execute(Button source) {
-        if (source.isHighlightOn()) {
-            source.background = new QuadBackgroundComponent(color(0.282, 0.463, 0.718, 1))
+        if (source.highlightOn) {
+            source.background = new QuadBackgroundComponent(buttonToolBarHighlightColor)
         } else {
-            source.background = null
+            source.background = new QuadBackgroundComponent(buttonToolBarColor)
         }
     }
 }
-
 def toolbarCmds = [
         (ButtonAction.Down)        : [toolbarPressedCmd],
         (ButtonAction.Up)          : [toolbarPressedCmd],
@@ -160,7 +190,7 @@ def toolbarCmds = [
 
 selector("toolbar", "button", "editor-style") {
     insets = new Insets3f(4, 4, 4, 0)
-    background = new QuadBackgroundComponent(color(0.137, 0.137, 0.137, 1))
+    background = new QuadBackgroundComponent(buttonToolBarColor)
     buttonCommands = toolbarCmds
 }
 
@@ -168,11 +198,11 @@ selector("toolbar", "button", "editor-style") {
 // Window
 //
 selector("window", "editor-style") {
-    background = new QuadBackgroundComponent(color(0.294, 0.294, 0.294, 1))
+    background = new QuadBackgroundComponent(windowColor)
 }
 
 selector("window", "title-wrapper", "editor-style") {
-    background = new QuadBackgroundComponent(color(0.2, 0.2, 0.2, 1))
+    background = new QuadBackgroundComponent(windowTitleColor)
 }
 
 selector("window", "title", "editor-style") {
@@ -202,11 +232,7 @@ selector("open-file", "textField", "editor-style") {
 
 selector("open-file", "list.container", "editor-style") {
     insets = new Insets3f(0, 8, 0, 8)
-    background = new QuadBackgroundComponent(color(0.2, 0.2, 0.2, 1))
-}
-
-selector("open-file", "list.container", "editor-style") {
-    background = new QuadBackgroundComponent(color(0.2, 0.2, 0.2, 1))
+    background = new QuadBackgroundComponent(listColor)
 }
 
 selector("open-file.list", "label", "editor-style") {
