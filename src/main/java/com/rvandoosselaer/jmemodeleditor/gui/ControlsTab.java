5ababb42c44a81ca4c849665aff0ddf8f79e9f69
rvandoosselaer <remy.vand@gmail.com>
2020-04-03 21:41:02 +0200

    refactor the controls tab

--- src/main/java/com/rvandoosselaer/jmemodeleditor/gui/ControlsTab.java
+++ src/main/java/com/rvandoosselaer/jmemodeleditor/gui/ControlsTab.java
@@ -3,7 +3,10 @@ package com.rvandoosselaer.jmemodeleditor.gui;
 import com.jme3.anim.AnimComposer;
 import com.jme3.anim.Joint;
 import com.jme3.anim.SkinningControl;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector2f;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
 import com.jme3.scene.control.Control;
 import com.rvandoosselaer.jmeutils.ApplicationGlobals;
@@ -13,15 +16,19 @@ import com.simsilica.lemur.Button;
 import com.simsilica.lemur.Command;
 import com.simsilica.lemur.Container;
 import com.simsilica.lemur.FillMode;
+import com.simsilica.lemur.HAlignment;
 import com.simsilica.lemur.Insets3f;
 import com.simsilica.lemur.Label;
 import com.simsilica.lemur.ListBox;
 import com.simsilica.lemur.Panel;
+import com.simsilica.lemur.VAlignment;
+import com.simsilica.lemur.component.IconComponent;
 import com.simsilica.lemur.component.SpringGridLayout;
 import jme3utilities.debug.SkeletonVisualizer;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
@@ -57,22 +64,18 @@ public class ControlsTab extends Tab {
                 container.addChild(new Label(GuiTranslations.getInstance().t("common.animcomposer"), PropertiesPanel.ELEMENT_ID.child("title")));
 
                 container.addChild(createStringListBox(GuiTranslations.getInstance().t("panel.properties.controls.anim.names"), new ArrayList<>(((AnimComposer) control).getAnimClipsNames())));
-                final ListBox<String>[] listBox = new ListBox[1];
-                container.depthFirstTraversal(spatial -> {
-                    if (spatial instanceof ListBox) {
-                        listBox[0] = (ListBox<String>) spatial;
-                    }
-                });
 
-                Button play = container.addChild(new Button("play"));
-                play.addClickCommands(cmd -> {
-                    Integer index = listBox[0].getSelectionModel().getSelection();
-                    if (index != null && index >= 0 && index < listBox[0].getModel().size()) {
-                        ((AnimComposer) control).setCurrentAction(listBox[0].getModel().get(index));
-                    }
-                });
-                Button stop = container.addChild(new Button("stop"));
-                stop.addClickCommands(cmd -> ((AnimComposer) control).reset());
+                Optional<ListBox<String>> listBox = getListBox(container);
+                if (listBox.isPresent()) {
+                    Container animButtons = container.addChild(new Container(new SpringGridLayout(Axis.X, Axis.Y, FillMode.First, FillMode.None))); // add in the seconds column
+                    animButtons.addChild(new Panel(1, 1, ColorRGBA.BlackNoAlpha)); // spacer
+                    Button play = animButtons.addChild(createIconButton("/Interface/play.png"));
+                    play.addClickCommands(cmd -> getListBoxSelection(listBox.get()).ifPresent(((AnimComposer) control)::setCurrentAction));
+                    Button stop = animButtons.addChild(createIconButton("/Interface/stop.png"));
+                    stop.addClickCommands(cmd -> ((AnimComposer) control).reset());
+                    // add a right margin to the last button, the size of the slider.
+                    stop.setInsets(new Insets3f(2, 0, 2, 2 + listBox.get().getSlider().getPreferredSize().x));
+                }
             } else if (control instanceof SkinningControl) {
                 container.addChild(new Label(GuiTranslations.getInstance().t("common.skinningcontrol"), PropertiesPanel.ELEMENT_ID.child("title")));
                 container.addChild(createBooleanInput(GuiTranslations.getInstance().t("panel.properties.controls.skinning.skeleton"),
@@ -90,6 +93,43 @@ public class ControlsTab extends Tab {
             container.addChild(createSeparator());
         }
         return container;
+    }
+
+    private static Button createIconButton(String iconPath) {
+        Button button = new Button("");
+        button.setInsets(new Insets3f(2, 0, 2, 2));
+        IconComponent icon = new IconComponent(iconPath);
+        icon.setIconSize(new Vector2f(12, 12));
+        icon.setMargin(4, 4);
+        icon.setHAlignment(HAlignment.Center);
+        icon.setVAlignment(VAlignment.Center);
+        button.setIcon(icon);
+
+        return button;
+    }
+
+    private Optional<ListBox<String>> getListBox(Spatial spatial) {
+        if (spatial instanceof ListBox) {
+            ListBox<String> listBox = (ListBox<String>) spatial;
+            return Optional.of(listBox);
+        }
+
+        if (spatial instanceof Node) {
+            for (Spatial s : ((Node) spatial).getChildren()) {
+                //return getListBox(s);
+                Optional<ListBox<String>> result = getListBox(s);
+                if (result.isPresent()) {
+                    return result;
+                }
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    private Optional<String> getListBoxSelection(ListBox<String> listBox) {
+        Integer index = listBox.getSelectionModel().getSelection();
+        return Optional.ofNullable(index != null ? listBox.getModel().get(index) : null);
     }
 
     private void showSkeleton(boolean visible, SkinningControl skinningControl) {
